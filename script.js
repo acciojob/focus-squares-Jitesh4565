a5ca0d9e09e0b709dfba0cf11fcb97b4fa889f7b
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-27 21:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function changeColor(event){
+	let hoveredSquareId=event.target.id;
+
+	let squares=document.querySelectorAll('.square');
+
+	squares.forEach(function(square){
+		if(square.id!==hoveredSquareId){
+			square.style.backgroundColor='#6F4E37';
+		}
+	});
+}
+  let squares=document.querySelectorAll('.square');
+    squares.forEach(function(square){
+	  square.addEventListener('mouseover',changeColor);
+	});
